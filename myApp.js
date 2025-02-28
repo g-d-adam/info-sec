0cdf0c9c193b60fef3f3ac3846d02b8740608d61
Adam ELFARISSI <adamelfarissi1@gmail.com>
2025-02-28 19:49:23 +0000

    trying to copy pass from claud

--- myApp.js
+++ myApp.js
@@ -2,7 +2,10 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-
+app.use(helmet());
+app.use(helmet({
+  strictTransportSecurity: false,
+}));
 
 
 
@@ -62,3 +65,4 @@ let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Useful programmer info security app started on port ${port}`);
 });
+module.exports = app;
