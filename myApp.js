e22120f506523d7936c3bc0f60098dc2d089b12e
Adam ELFARISSI <adamelfarissi1@gmail.com>
2025-03-03 06:55:02 +0000

    challenge 2

--- myApp.js
+++ myApp.js
@@ -61,8 +61,9 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
+let port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Useful programmer info security app started on port ${port}`);
 });
 module.exports = app;
+helmet.hidePoweredBy();
